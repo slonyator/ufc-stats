765134509a204b59702d4cf53135da2008e6946e
Michael Polonskij <michael.polonskij@gmx.de>
2023-12-29 19:40:19 +0100

    feat: links f/ events are also provided

--- scrapy_event_loader.py
+++ scrapy_event_loader.py
@@ -2,7 +2,6 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 import pandas as pd
 
-# Define the Scrapy spider
 class UfcEventsSpider(scrapy.Spider):
     name = 'ufc_events'
     start_urls = ['http://ufcstats.com/statistics/events/completed']
@@ -12,31 +11,30 @@ class UfcEventsSpider(scrapy.Spider):
 
     def parse(self, response):
         for event_row in response.xpath('//tr[contains(@class, "b-statistics__table-row")]'):
-            link = event_row.xpath('.//a/@href').get()
-            event_name = event_row.xpath('.//a/text()').get()
-            event_date = event_row.xpath('.//span[@class="b-statistics__date"]/text()').get()
-            location = event_row.xpath('following-sibling::tr[1]/td/text()').get()
+            link = event_row.xpath('.//a/@href').extract_first()
+            event_name = event_row.xpath('.//a/text()').extract_first()
+            event_date = event_row.xpath('.//span[@class="b-statistics__date"]/text()').extract_first()
+            location = event_row.xpath('following-sibling::tr[1]/td/text()').extract_first()
 
-            if link and event_name and event_date:
-                item = {
-                    'event_name': event_name.strip(),
-                    'date': event_date.strip(),
-                    'location': location.strip() if location else 'Unknown'
-                }
-                print(item)
-                self.results_list.append(item)
-                yield item
+            item = {
+                'event_name': event_name.strip() if event_name else '',
+                'date': event_date.strip() if event_date else '',
+                'location': location.strip() if location else 'Unknown',
+                'link': link.strip() if link else ''
+            }
+            self.results_list.append(item)
+            yield item
 
-        next_page = response.css('a.b-link_style_black[href*="page"]:last-child::attr(href)').get()
-        if next_page:
+        # Pagination
+        next_page = response.css('a.b-link_style_black::attr(href)').extract()[-1]
+        if next_page and response.urljoin(next_page) != response.url:
             yield response.follow(next_page, self.parse)
 
-# Function to run the spider and collect the data
 def run_spider():
     scraped_items = []
     process = CrawlerProcess(settings={'LOG_LEVEL': 'DEBUG'})
     process.crawl(UfcEventsSpider, results_list=scraped_items)
-    process.start()  # This will block until the spider is finished
+    process.start()
     return scraped_items
 
 if __name__ == "__main__":
