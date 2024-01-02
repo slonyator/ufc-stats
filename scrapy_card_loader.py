18e6e482b720aac70438d101688e54fd0cb60664
Michael Polonskij <michael.polonskij@gmx.de>
2024-01-02 12:46:26 +0100

    feat: Extension to scrape multiple websites
    Now we can pass a list of websites from "outside" and use the webscraping
    functionality from the UfcCardSpider class.

--- scrapy_card_loader.py
+++ scrapy_card_loader.py
@@ -4,43 +4,52 @@ import pandas as pd
 
 class UfcCardSpider(scrapy.Spider):
     name = 'ufc_card_spider'
-    start_urls = ['http://ufcstats.com/event-details/a8e8587a06e73c87']
 
-    def __init__(self, results):
+    def __init__(self, urls, results):
+        self.start_urls = urls
         self.results = results
 
     def parse(self, response):
-        # Extracting headers
         headers = response.css('thead.b-fight-details__table-head > tr > th::text').getall()
         headers = [header.strip() for header in headers]
 
-        # Extracting rows
         for row in response.css('tbody.b-fight-details__table-body > tr'):
             row_data = []
             for i in range(len(headers)):
                 cell_data = row.css('td:nth-child({}) *::text'.format(i + 1)).getall()
                 cell_data = ' '.join(text.strip() for text in cell_data)
                 row_data.append(cell_data.strip())
-            self.results.append(dict(zip(headers, row_data)))
+            self.results[response.url].append(dict(zip(headers, row_data)))
 
 class UfcCrawler:
-    def scrape_and_convert_to_dataframe(self):
-        results = []
+    def scrape_multiple_sites(self, urls):
+        results = {url: [] for url in urls}
         process = CrawlerProcess({
             'USER_AGENT': 'Mozilla/5.0 (compatible; Scrapy/1.0; +http://scrapy.org)'
         })
 
-        process.crawl(UfcCardSpider, results=results)
-        process.start()  # the script will block here until the crawling is finished
+        process.crawl(UfcCardSpider, urls=urls, results=results)
+        process.start()
 
-        # Convert the results into a DataFrame
-        if results:
-            df = pd.DataFrame(results)
-            return df
-        else:
-            return pd.DataFrame()
+        dataframes = {}
+        for url, data in results.items():
+            if data:
+                df = pd.DataFrame(data)
+                dataframes[url] = df
+            else:
+                dataframes[url] = pd.DataFrame()
+
+        return dataframes
 
 if __name__ == "__main__":
+    urls = [
+        "http://ufcstats.com/event-details/a8e8587a06e73c87",
+        # Add more URLs here
+    ]
     crawler = UfcCrawler()
-    df = crawler.scrape_and_convert_to_dataframe()
-    print(df)
+    results = crawler.scrape_multiple_sites(urls)
+
+    for url, df in results.items():
+        print(f"Data from {url}:\n", df)
+
+    print("ready!")
